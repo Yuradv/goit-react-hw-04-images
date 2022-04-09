f633de2306f3671d6adfcf4792630dd419f23a0c
Yura Dolzhnikov <yuradolzhnikov@gmail.com>
2022-04-09 20:19:37 +0300

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -18,18 +18,3 @@ const loader = () => {
 };
 
 export default loader;
-
-// import { TailSpin } from 'react-loader-spinner';
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-
-// import s from './Loader.module.css';
-
-// const loader = () => {
-//   return (
-//     <div className={s.loader}>
-//       {<TailSpin height="80" width="80" color="00BFFF" ariaLabel="loading" />}
-//     </div>
-//   );
-// };
-
-// export default loader;
